6c19a0f265e94f7c1c322b0f49df264c099d7bab
ubuntu <igor@oracles.org>
2017-11-23 16:39:00 -0800

    added travis integration

---
+++ test.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Running Parity Full Test Suite
+
+FEATURES="json-tests"
+OPTIONS="--release"
+
+case $1 in
+    --no-json)
+    FEATURES="ipc"
+    shift # past argument=value
+    ;;
+	--no-release)
+	OPTIONS=""
+	shift
+	;;
+	--no-run)
+	OPTIONS="--no-run"
+	shift
+	;;
+    *)
+            # unknown option
+    ;;
+esac
+
+set -e
+
+./scripts/validate_chainspecs.sh
+
+cargo test -j 8 $OPTIONS --features "$FEATURES" --all --exclude evmjit $1
